f92371618396e19f152d094fe4f10fe2b45a3d97
Ruslana Babich <r.babich@g.nsu.ru>
2026-07-03 15:54:13 +0700

    refactor: remove unused extension

--- app/src/main/java/com/rasymptote/pizzashiftintensive/presentation/extension/StringExt.kt
+++
@@ -1,6 +0,0 @@
-package com.rasymptote.pizzashiftintensive.presentation.extension
-
-fun String.capitalizeFirst(): String =
-    replaceFirstChar {
-        if (it.isLowerCase()) it.titlecase() else it.toString()
-    }
