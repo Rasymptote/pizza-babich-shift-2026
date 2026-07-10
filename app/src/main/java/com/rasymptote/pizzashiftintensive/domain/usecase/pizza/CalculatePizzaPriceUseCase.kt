c2214e6937919b2a12e89548f511e74ef50f4dee
Ruslana Babich <r.babich@g.nsu.ru>
2026-07-10 17:36:33 +0700

    refactor: use sumOf for toppings price calculation

--- app/src/main/java/com/rasymptote/pizzashiftintensive/domain/usecase/pizza/CalculatePizzaPriceUseCase.kt
+++ app/src/main/java/com/rasymptote/pizzashiftintensive/domain/usecase/pizza/CalculatePizzaPriceUseCase.kt
@@ -3,13 +3,8 @@ package com.rasymptote.pizzashiftintensive.domain.usecase.pizza
 import com.rasymptote.pizzashiftintensive.domain.model.pizza.PizzaConfiguration
 
 class CalculatePizzaPriceUseCase {
-    operator fun invoke(pizzaConfiguration: PizzaConfiguration) : Int {
-        var price = pizzaConfiguration.selectedSize.price
-        price += pizzaConfiguration.selectedDough.price
-        pizzaConfiguration.selectedToppings.forEach {
-            price += it.price
-        }
-
-        return price
-    }
+    operator fun invoke(pizzaConfiguration: PizzaConfiguration): Int =
+        pizzaConfiguration.selectedSize.price +
+                pizzaConfiguration.selectedDough.price +
+                pizzaConfiguration.selectedToppings.sumOf { it.price }
 }
